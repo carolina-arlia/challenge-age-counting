93054436b50e87edc02aa0b752ee8f85c0846c9b
Carolina Arlia <carolinaiarlia@gmail.com>
2022-09-29 22:05:13 +0200

    new challenge

---
+++ age-counting.rb
@@ -0,0 +1,18 @@
+require "json"
+require "open-uri"
+
+url = "https://coderbyte.com/api/challenges/json/age-counting"
+data = JSON.parse(URI.open(url).read)
+
+array_data = data["data"].split(",")
+
+counter = 0
+
+array_data.each_with_index do |element, index|
+  if index.odd? && element.partition("=").last.to_i >= 50
+    counter += 1
+  end
+end
+
+
+puts counter
